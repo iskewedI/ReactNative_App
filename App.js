f290956f0422daf33304befd6a8c9e302d0b3b8c
iskewedi <jtornello2019@hotmail.com>
2021-06-30 11:27:10 -0300

    Added Texts, Images and Buttons (blow up logic)

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+  // Only for ANDROID => Works only with a View (div) inside that has a BACKGROUND COLOR.
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+} from 'react-native';
 
 export default App = () => {
   let x = 1;
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
+    // SafeAreaView => On iOS, this will add top-padding to maintain content below the Status Bar.
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2}>Hello world!</Text>
+      <TouchableHighlight
+        onPress={() =>
+          Alert.alert('Alert', 'Image pressed.', [
+            { text: "I'm sorry." },
+            {
+              text: 'Blow up.',
+              onPress: () => {
+                console.log('Blowing up...');
+                Alert.alert('Last words', 'Say your last words.', [
+                  {
+                    text: "I'm ready.",
+                    onPress: () => Alert.alert('Explosion.', 'Boom.'),
+                  },
+                ]);
+              },
+            },
+          ])
+        }
+      >
+        <Image
+          source={{ uri: 'https://picsum.photos/200/300', width: 200, height: 300 }}
+        />
+      </TouchableHighlight>
+      <Button title='Click me' onPress={() => alert('Button pressed.')} color='red' />
       <StatusBar style='auto' />
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -16,6 +51,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    // Work around for Android devices (SafeAreaView don't work)
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
     alignItems: 'center',
     justifyContent: 'center',
   },
