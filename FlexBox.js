05b1c532ec46d6e5f2b6c872dea6b246f2b57b43
Joaquin <jtornello2019@hotmail.com>
2021-07-01 18:12:20 -0300

    Added comments

--- FlexBox.js
+++ FlexBox.js
@@ -6,8 +6,17 @@ export default FlexBox = () => {
     <View
       style={{
         backgroundColor: '#fff',
-        flex: 1,
         // flex: .5, //Half of the screen
+        flex: 1,
+        // By default, flexDirection is COLUMN.
+        flexDirection: 'row',
+        //  alignItems: Set the placement of each items.
+        /* The "base" of the secondary axis will setted in the same place, 
+           beyond what height or width they have. Used mostly in GRAPHS. */
+        alignItems: 'baseline',
+        /* alignContent only works if flexWrap is WRAP. Set the aligment to the
+           entire content. */
+        alignContent: '',
       }}
     >
       <View
